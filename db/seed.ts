b43b98c6ceef5cc779877de7459fa3d27c0ffea2
Germán Castro <patamembrillo@proton.me>
2024-11-29 10:21:51 +0100

    real seeding

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,5 @@
 import { seed } from "drizzle-seed";
+import { faker } from '@faker-js/faker';
 import { db } from "db";
 import * as schema from "./schema";
 
@@ -7,8 +8,33 @@ async function main() {
   // clear the database
   await db.delete(schema.componentsTable);
 
-  // TODO: Add refine seeding
-  await seed(db, schema, { count: 10 });
+
+  // Bulk insert components
+  await db.insert(schema.componentsTable).values(
+    Array.from({ length: 10 }, () => ({
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      developer: faker.internet.username(),
+      v0Url: faker.internet.url(),
+      githubUrl: faker.internet.url(),
+      siteUrl: faker.internet.url(),
+      imageUrl: faker.image.url(),
+    })) 
+  );
+
+  // await seed(db, schema, { count: 10 }).refine((f) => ({
+  //   componentsTable: {
+  //     columns: {
+  //       name: f.companyName(),
+  //       description: f.loremIpsum(),
+  //       developer: f.firstName(),
+  //       v0Url: f.string(),
+  //       githubUrl: f.string(),
+  //       siteUrl: f.string(),
+  //       imageUrl: f.string(),
+  //     }
+  //   }
+  // }))
 }
 
 main();
